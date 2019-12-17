29c642a44a341f9bc2af8114745d1e15a374f31a
cosmeticsonMW <jermainharhuis@gmail.com>
2019-12-17 14:46:53 +0100

    Added MainMenu,Settigns

---
+++ unity/XmasJam2019/Assets/Scripts/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Settings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void MainMenu()
+
+    {
+
+        SceneManager.LoadScene(2);
+        
+    }
+
+    public void SettingsMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
